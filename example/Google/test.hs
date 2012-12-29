a027b8ba1ee9bb818decfbf3c263e2a8b1579769
Gert Cuykens <gert.cuykens@gmail.com>
2012-12-29 23:09:32 +0100

    debug json

--- example/Google/test.hs
+++ example/Google/test.hs
@@ -1,4 +1,4 @@
-{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE OverloadedStrings, TemplateHaskell #-}
 
 {-
 
@@ -21,6 +21,37 @@ import Network.OAuth.OAuth2.HttpClient
 import Network.OAuth.OAuth2
 import Google.Key
 
+import Control.Monad.IO.Class (MonadIO, liftIO)
+import Data.Aeson (FromJSON)
+import Data.Aeson.TH (deriveJSON)
+import qualified Data.ByteString.Char8 as BS
+import qualified Data.ByteString.Lazy.Internal as BL
+import Data.Maybe (fromMaybe)
+import Data.Text (Text)
+import Network.HTTP.Conduit (Response)
+import System.Environment
+
+--------------------------------------------------
+
+data Token = Token { issued_to::Text
+                   , audience::Text
+                   , scope::Text
+                   , expires_in::Text
+                   , email::Text
+                   , verified_email::Text
+                   , access_type::Text} deriving (Show)
+$(deriveJSON id ''Token)
+
+data User = User { uid::Text
+                 , name::Text
+                 , given_name::Text
+                 , family_name::Text
+                 , link::Text
+                 , picture::Text
+                 , gender::Text
+                 , birthday::Text
+                 , locale::Text} deriving (Show)
+$(deriveJSON id ''User)
 
 --------------------------------------------------
 
@@ -31,55 +62,62 @@ main = do
         "normal" -> normalCase
         "offline" -> offlineCase
 
-
 offlineCase :: IO ()
-offlineCase = do 
-          print $ authorizationUrl googleKeys `BS.append` "&" `BS.append` extraParams
+offlineCase = do
+          print $ authorizationUrl googleKeys `appendQueryParam'` googleScopeEmail `appendQueryParam'` googleAccessOffline
           putStrLn "visit the url and paste code here: "
           code <- getLine
-          (Just (AccessToken accessToken refreshToken)) <- requestAccessToken googleKeys (BS.pack code) 
+          (Just (AccessToken accessToken refreshToken)) <- requestAccessToken googleKeys (BS.pack code)
           print (accessToken, refreshToken)
           validateToken accessToken >>= print
-          -- 
+          --
           -- obtain a new access token with refresh token, which turns out only in response at first time.
           -- Revoke Access https://www.google.com/settings/security
           --
           case refreshToken of
             Nothing -> print "Failed to fetch refresh token"
             Just tk -> refreshAccessToken googleKeys tk >>= print
-    where extraParams = renderSimpleQuery False $ ("access_type", "offline"):googleScopeEmail
-
 
 normalCase :: IO ()
-normalCase = do 
-          print $ authorizationUrl googleKeys `BS.append` "&" `BS.append` extraParams
+normalCase = do
+          print $ authorizationUrl googleKeys `appendQueryParam'` googleScopeUserInfo
           putStrLn "visit the url and paste code here: "
           code <- getLine
-          (Just (AccessToken accessToken Nothing)) <- requestAccessToken googleKeys (BS.pack code) 
+          (Just (AccessToken accessToken Nothing)) <- requestAccessToken googleKeys (BS.pack code)
           print accessToken
-          res <- validateToken accessToken
-          print res
-          res2 <- userinfo accessToken
-          print res2
-    where extraParams = renderSimpleQuery False googleScopeEmail
-
+          validateToken accessToken >>= print
+          (validateToken' accessToken :: IO (Maybe Token)) >>= print
+          userinfo accessToken >>= print
+          (userinfo' accessToken :: IO (Maybe User)) >>= print
 
 --------------------------------------------------
 -- Google API
 
--- | this is special for google Gain read-only access to the user's email address.
+-- | This is special for google Gain read-only access to the user's email address.
+googleScopeEmail :: QueryParams
 googleScopeEmail = [("scope", "https://www.googleapis.com/auth/userinfo.email")]
 
--- | Gain read-only access to basic profile information, including a 
+-- | Gain read-only access to basic profile information, including a
+googleScopeUserInfo :: QueryParams
 googleScopeUserInfo = [("scope", "https://www.googleapis.com/auth/userinfo.profile")]
 
+-- | Access offline
+googleAccessOffline :: QueryParams
+googleAccessOffline = [("access_type", "offline")]
+
 -- Token Validation
-validateToken accessToken = doSimplePostRequest ("https://www.googleapis.com/oauth2/v1/tokeninfo", 
-                                                 (accessTokenToParam accessToken))
+validateToken :: BS.ByteString -> IO BL.ByteString
+validateToken accessToken = doSimplePostRequest ("https://www.googleapis.com/oauth2/v1/tokeninfo", (accessTokenToParam accessToken))
+
+validateToken' :: FromJSON a => BS.ByteString -> IO (Maybe a)
+validateToken' accessToken = doJSONGetRequest ("https://www.googleapis.com/oauth2/v1/tokeninfo" `appendQueryParam` (accessTokenToParam accessToken))
 
 -- | fetch user email.
 --   for more information, please check the playround site.
 --
-userinfo accessToken = doSimpleGetRequest (appendQueryParam
-                                           "https://www.googleapis.com/oauth2/v2/userinfo"
-	                                   (accessTokenToParam accessToken))
+userinfo :: BS.ByteString -> IO BL.ByteString
+userinfo accessToken = doSimpleGetRequest (appendQueryParam "https://www.googleapis.com/oauth2/v2/userinfo" (accessTokenToParam accessToken))
+
+userinfo' :: FromJSON a => BS.ByteString -> IO (Maybe a)
+userinfo' accessToken = doJSONGetRequest ("https://www.googleapis.com/oauth2/v2/userinfo" `appendQueryParam` (accessTokenToParam accessToken))
+
