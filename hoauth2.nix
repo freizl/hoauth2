962aa927ab740799e82a536c42c71fba18eab199
haisheng.wu <freizl@gmail.com>
2019-10-16 17:31:02 -0700

    update nix config

--- hoauth2.nix
+++ hoauth2.nix
@@ -1,16 +1,17 @@
-{ mkDerivation, aeson, base, bytestring, exceptions, http-conduit
-, http-types, microlens, stdenv, text, unordered-containers
-, uri-bytestring, uri-bytestring-aeson
+{ mkDerivation, aeson, base, binary, bytestring, exceptions
+, http-conduit, http-types, microlens, stdenv, text
+, unordered-containers, uri-bytestring, uri-bytestring-aeson
 }:
 mkDerivation {
   pname = "hoauth2";
-  version = "1.8.9";
-  src = ./.;
+  version = "1.9.0";
+  sha256 = "1br1g6xp0s73aj8nsx3rjdpji3lvp19b4xaxhn87fqbnnhczg39z";
   isLibrary = true;
   isExecutable = true;
   libraryHaskellDepends = [
-    aeson base bytestring exceptions http-conduit http-types microlens
-    text unordered-containers uri-bytestring uri-bytestring-aeson
+    aeson base binary bytestring exceptions http-conduit http-types
+    microlens text unordered-containers uri-bytestring
+    uri-bytestring-aeson
   ];
   homepage = "https://github.com/freizl/hoauth2";
   description = "Haskell OAuth2 authentication client";
