17a889b77dd218b4acef83fa426c25f8a74a91b1
Haisheng Wu mac16 <freizl@gmail.com>
2021-09-09 15:00:51 -0700

    Remove `IDP a` constraint in the IDPApp data type
    - also update comment
    - auto format by IDE

--- example/IDP/Weibo.hs
+++ example/IDP/Weibo.hs
@@ -1,21 +1,22 @@
-{-# LANGUAGE DeriveGeneric     #-}
+{-# LANGUAGE DeriveGeneric #-}
 {-# LANGUAGE OverloadedStrings #-}
-{-# LANGUAGE QuasiQuotes       #-}
+{-# LANGUAGE QuasiQuotes #-}
 
 module IDP.Weibo where
-import           Data.Aeson
-import           Data.Bifunctor
+
+import Data.Aeson
+import Data.Bifunctor
 import qualified Data.ByteString.Lazy.Char8 as BSL
-import           Data.Hashable
-import           Data.Text.Lazy             (Text)
-import qualified Data.Text.Lazy             as TL
-import           GHC.Generics
-import           Keys
-import           Network.OAuth.OAuth2
-import           Types
-import           URI.ByteString
-import           URI.ByteString.QQ
-import           Utils
+import Data.Hashable
+import Data.Text.Lazy (Text)
+import qualified Data.Text.Lazy as TL
+import GHC.Generics
+import Keys
+import Network.OAuth.OAuth2
+import Types
+import URI.ByteString
+import URI.ByteString.QQ
+import Utils
 
 data Weibo = Weibo deriving (Show, Generic)
 
@@ -40,25 +41,31 @@ instance HasUserReq Weibo where
     return (re >>= (bimap BSL.pack toLoginUser . eitherDecode))
 
 instance HasAuthUri Weibo where
-  authUri _ = createCodeUri weiboKey [ ("state", "Weibo.test-state-123")
-                                        ]
+  authUri _ =
+    createCodeUri
+      weiboKey
+      [ ("state", "Weibo.test-state-123")
+      ]
 
--- TODO: http://open.weibo.com/wiki/2/users/show
-data WeiboUser = WeiboUser { id         :: Integer
-                           , name       :: Text
-                           , screenName :: Text
-                           } deriving (Show, Generic)
+-- | UserInfor API: http://open.weibo.com/wiki/2/users/show
+data WeiboUser = WeiboUser
+  { id :: Integer,
+    name :: Text,
+    screenName :: Text
+  }
+  deriving (Show, Generic)
 
-newtype WeiboUID = WeiboUID { uid :: Integer }
+newtype WeiboUID = WeiboUID {uid :: Integer}
   deriving (Show, Generic)
 
 instance FromJSON WeiboUID where
-    parseJSON = genericParseJSON defaultOptions { fieldLabelModifier = camelTo2 '_' }
+  parseJSON = genericParseJSON defaultOptions {fieldLabelModifier = camelTo2 '_'}
+
 instance FromJSON WeiboUser where
-    parseJSON = genericParseJSON defaultOptions { fieldLabelModifier = camelTo2 '_' }
+  parseJSON = genericParseJSON defaultOptions {fieldLabelModifier = camelTo2 '_'}
 
 userInfoUri :: URI
 userInfoUri = [uri|https://api.weibo.com/2/account/get_uid.json|]
 
 toLoginUser :: WeiboUID -> LoginUser
-toLoginUser ouser = LoginUser { loginUserName = TL.pack $ show $ uid ouser }
+toLoginUser ouser = LoginUser {loginUserName = TL.pack $ show $ uid ouser}
--- example/Types.hs
+++ example/Types.hs
@@ -1,34 +1,33 @@
-{-# LANGUAGE AllowAmbiguousTypes       #-}
-{-# LANGUAGE DeriveGeneric             #-}
+{-# LANGUAGE AllowAmbiguousTypes #-}
+{-# LANGUAGE DeriveGeneric #-}
 {-# LANGUAGE ExistentialQuantification #-}
-{-# LANGUAGE OverloadedStrings         #-}
-{-# LANGUAGE RankNTypes                #-}
-{-# LANGUAGE TypeFamilies              #-}
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE RankNTypes #-}
+{-# LANGUAGE TypeFamilies #-}
 
 module Types where
 
-import           Control.Concurrent.MVar
-import           Data.Aeson
-import qualified Data.ByteString.Lazy.Char8        as BSL
-import           Data.Hashable
-import qualified Data.HashMap.Strict               as Map
-import           Data.Maybe
-import           Data.Text.Lazy
-import qualified Data.Text.Lazy                    as TL
-import           GHC.Generics
-import           Network.HTTP.Conduit
-import           Network.OAuth.OAuth2
+import Control.Concurrent.MVar
+import Data.Aeson
+import qualified Data.ByteString.Lazy.Char8 as BSL
+import qualified Data.HashMap.Strict as Map
+import Data.Hashable
+import Data.Maybe
+import Data.Text.Lazy
+import qualified Data.Text.Lazy as TL
+import GHC.Generics
+import Network.HTTP.Conduit
+import Network.OAuth.OAuth2
 import qualified Network.OAuth.OAuth2.TokenRequest as TR
-import           Text.Mustache
-import qualified Text.Mustache                     as M
+import Text.Mustache
+import qualified Text.Mustache as M
 
 type IDPLabel = Text
 
--- TODO: how to make following type work??
--- type CacheStore = forall a. IDP a => MVar (Map.HashMap a IDPData)
 type CacheStore = MVar (Map.HashMap IDPLabel IDPData)
 
 -- * type class for defining a IDP
+
 --
 class (Hashable a, Show a) => IDP a
 
@@ -51,32 +50,36 @@ class (IDP a) => HasUserReq a where
 -- Heterogenous collections
 -- https://wiki.haskell.org/Heterogenous_collections
 --
-data IDPApp = forall a. (IDP a,
-                         HasTokenRefreshReq a,
-                         HasTokenReq a,
-                         HasUserReq a,
-                         HasLabel a,
-                         HasAuthUri a) => IDPApp a
+data IDPApp
+  = forall a.
+    ( HasTokenRefreshReq a,
+      HasTokenReq a,
+      HasUserReq a,
+      HasLabel a,
+      HasAuthUri a
+    ) =>
+    IDPApp a
 
 -- dummy oauth2 request error
 --
-data Errors =
-  SomeRandomError
+data Errors
+  = SomeRandomError
   deriving (Show, Eq, Generic)
 
 instance FromJSON Errors where
-  parseJSON = genericParseJSON defaultOptions { constructorTagModifier = camelTo2 '_', allNullaryToStringTag = True }
+  parseJSON = genericParseJSON defaultOptions {constructorTagModifier = camelTo2 '_', allNullaryToStringTag = True}
 
-newtype LoginUser =
-  LoginUser { loginUserName :: Text
-            } deriving (Eq, Show)
+newtype LoginUser = LoginUser
+  { loginUserName :: Text
+  }
+  deriving (Eq, Show)
 
-data IDPData =
-  IDPData { codeFlowUri     :: Text
-          , loginUser       :: Maybe LoginUser
-          , oauth2Token     :: Maybe OAuth2Token
-          , idpDisplayLabel :: IDPLabel
-          }
+data IDPData = IDPData
+  { codeFlowUri :: Text,
+    loginUser :: Maybe LoginUser,
+    oauth2Token :: Maybe OAuth2Token,
+    idpDisplayLabel :: IDPLabel
+  }
 
 -- simplify use case to only allow one idp instance for now.
 instance Eq IDPData where
@@ -85,23 +88,29 @@ instance Eq IDPData where
 instance Ord IDPData where
   a `compare` b = idpDisplayLabel a `compare` idpDisplayLabel b
 
-newtype TemplateData = TemplateData { idpTemplateData :: [IDPData]
-                                    } deriving (Eq)
+newtype TemplateData = TemplateData
+  { idpTemplateData :: [IDPData]
+  }
+  deriving (Eq)
 
 -- * Mustache instances
+
 instance ToMustache IDPData where
-  toMustache t' = M.object
-    [ "codeFlowUri" ~> codeFlowUri t'
-    , "isLogin" ~> isJust (loginUser t')
-    , "user" ~> loginUser t'
-    , "name" ~> TL.unpack (idpDisplayLabel t')
-    ]
+  toMustache t' =
+    M.object
+      [ "codeFlowUri" ~> codeFlowUri t',
+        "isLogin" ~> isJust (loginUser t'),
+        "user" ~> loginUser t',
+        "name" ~> TL.unpack (idpDisplayLabel t')
+      ]
 
 instance ToMustache LoginUser where
-  toMustache t' = M.object
-    [ "name" ~> loginUserName t' ]
+  toMustache t' =
+    M.object
+      ["name" ~> loginUserName t']
 
 instance ToMustache TemplateData where
-  toMustache td' = M.object
-    [ "idps" ~> idpTemplateData td'
-    ]
+  toMustache td' =
+    M.object
+      [ "idps" ~> idpTemplateData td'
+      ]
