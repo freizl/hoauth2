ac160e3e44fb33bc3cce863e065a85a70922bfc3
Haisheng Wu mac16 <freizl@gmail.com>
2022-01-28 12:33:52 -0800

    add comments tod okta idp

--- example/IDP/Okta.hs
+++ example/IDP/Okta.hs
@@ -1,19 +1,20 @@
-{-# LANGUAGE DeriveGeneric     #-}
+{-# LANGUAGE DeriveGeneric #-}
 {-# LANGUAGE OverloadedStrings #-}
-{-# LANGUAGE QuasiQuotes       #-}
+{-# LANGUAGE QuasiQuotes #-}
 
 module IDP.Okta where
-import           Data.Aeson
-import           Data.Bifunctor
-import           Data.Hashable
-import           Data.Text.Lazy                 ( Text )
-import           GHC.Generics
-import           Keys
-import           Network.OAuth.OAuth2
-import           Types
-import           URI.ByteString
-import           URI.ByteString.QQ
-import           Utils
+
+import Data.Aeson
+import Data.Bifunctor
+import Data.Hashable
+import Data.Text.Lazy (Text)
+import GHC.Generics
+import Keys
+import Network.OAuth.OAuth2
+import Types
+import URI.ByteString
+import URI.ByteString.QQ
+import Utils
 
 data Okta = Okta
   deriving (Show, Generic)
@@ -35,28 +36,36 @@ instance HasUserReq Okta where
     re <- authGetJSON mgr at userInfoUri
     return (second toLoginUser re)
 
+-- | https://developer.okta.com/docs/reference/api/oidc/#request-parameters
+-- Okta Org AS doesn't support consent
+-- Okta Custom AS does support consent via config (what scope shall prompt consent)
+--
 instance HasAuthUri Okta where
-  authUri _ = createCodeUri
-    oktaKey
-    [ ("state", "Okta.test-state-123")
-    , ( "scope"
-      , "openid profile offline_access okta.users.read.self okta.users.read"
-      )
-    ]
+  authUri _ =
+    createCodeUri
+      oktaKey
+      [ ("state", "Okta.test-state-123"),
+        ( "scope",
+          "openid profile"
+          -- , "openid profile offline_access okta.users.read.self okta.users.read"
+        ),
+        ("prompt", "login consent")
+      ]
 
 data OktaUser = OktaUser
-  { name              :: Text
-  , preferredUsername :: Text
+  { name :: Text,
+    preferredUsername :: Text
   }
   deriving (Show, Generic)
 
 instance FromJSON OktaUser where
   parseJSON =
-    genericParseJSON defaultOptions { fieldLabelModifier = camelTo2 '_' }
+    genericParseJSON defaultOptions {fieldLabelModifier = camelTo2 '_'}
 
 userInfoUri :: URI
-userInfoUri = [uri|http://rain.okta1.com:1802/oauth2/v1/userinfo|]
+userInfoUri = [uri|https://hw2.trexcloud.com/oauth2/v1/userinfo|]
+
+-- userInfoUri = [uri|https://dev-148986.oktapreview.com/oauth2/v1/userinfo|]
 
 toLoginUser :: OktaUser -> LoginUser
-toLoginUser ouser = LoginUser { loginUserName = name ouser }
-
+toLoginUser ouser = LoginUser {loginUserName = name ouser}
