2c4a592622984eee8787383e5cb5dc95354054f5
HaishengWuVBox <freizl@gmail.com>
2022-02-18 20:37:36 +0000

    add nix files

---
+++ hoauth2-example/hoauth2-example.nix
@@ -0,0 +1,21 @@
+{ mkDerivation, aeson, base, binary, bytestring, directory
+, hashable, hoauth2, http-conduit, http-types, lib, microlens
+, mustache, parsec, scotty, text, unordered-containers
+, uri-bytestring, wai, wai-middleware-static, warp
+}:
+mkDerivation {
+  pname = "hoauth2-example";
+  version = "1.0.0";
+  src = ./.;
+  isLibrary = false;
+  isExecutable = true;
+  enableSeparateDataOutput = true;
+  executableHaskellDepends = [
+    aeson base binary bytestring directory hashable hoauth2
+    http-conduit http-types microlens mustache parsec scotty text
+    unordered-containers uri-bytestring wai wai-middleware-static warp
+  ];
+  homepage = "https://github.com/freizl/hoauth2";
+  description = "OAuth2 example";
+  license = lib.licenses.bsd3;
+}
