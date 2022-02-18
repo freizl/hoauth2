1f54782347355205c0ac2bb4a5780ac18b548ba7
Haisheng Wu mac16 <freizl@gmail.com>
2022-02-18 12:41:34 -0800

    add 2 packages to default.nix

--- default.nix
+++ default.nix
@@ -2,6 +2,7 @@ let
   pkgs = import <nixpkgs> { };
 
 in
-  { hoauth2 = pkgs.haskellPackages.callPackage ./hoauth2.nix { } ;
+  { hoauth2 = pkgs.haskellPackages.callPackage ./hoauth2/hoauth2.nix { } ;
+    hoauth2-example = pkgs.haskellPackages.callPackage ./hoauth2-example/hoauth2-example.nix { } ;
   }
 
