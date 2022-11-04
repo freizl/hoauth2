251d58aaaf2a2c74760db5124ac2830ddfcc1cfc
Haisheng Wu (M) <freizl@gmail.com>
2022-11-04 16:44:29 -0700

    Update oauth2 and user info endpoint for Azure
    fixes #165

--- hoauth2-providers/src/Network/OAuth2/Provider/AzureAD.hs
+++ hoauth2-providers/src/Network/OAuth2/Provider/AzureAD.hs
@@ -15,29 +15,43 @@ data AzureAD = AzureAD deriving (Eq, Show)
 
 type instance IdpUserInfo AzureAD = AzureADUser
 
+-- create app at https://go.microsoft.com/fwlink/?linkid=2083908
+--
+-- also be aware to find the right client id.
+-- see https://stackoverflow.com/a/70670961
 defaultAzureADApp :: IdpApplication 'AuthorizationCode AzureAD
 defaultAzureADApp =
   AuthorizationCodeIdpApplication
     { idpAppClientId = ""
     , idpAppClientSecret = ""
-    , idpAppScope = Set.empty
+    , idpAppScope = Set.fromList ["openid", "profile", "email"]
     , idpAppAuthorizeState = "CHANGE_ME"
-    , idpAppAuthorizeExtraParams = Map.fromList [("resource", "https://graph.microsoft.com")]
+    , idpAppAuthorizeExtraParams = Map.empty
     , idpAppRedirectUri = [uri|http://localhost|]
-    , idpAppName = "default-azure-App"
+    , idpAppName = "default-azure-app"
     , idpAppTokenRequestAuthenticationMethod = ClientSecretBasic
     , idp = defaultAzureADIdp
     }
 
+-- | https://login.microsoftonline.com/common/v2.0/.well-known/openid-configuration
 defaultAzureADIdp :: Idp AzureAD
 defaultAzureADIdp =
   Idp
     { idpFetchUserInfo = authGetJSON @(IdpUserInfo AzureAD)
-    , idpUserInfoEndpoint = [uri|https://graph.microsoft.com/v1.0/me|]
-    , idpAuthorizeEndpoint = [uri|https://login.windows.net/common/oauth2/authorize|]
-    , idpTokenEndpoint = [uri|https://login.windows.net/common/oauth2/token|]
+    , idpUserInfoEndpoint = [uri|https://graph.microsoft.com/oidc/userinfo|]
+    , idpAuthorizeEndpoint = [uri|https://login.microsoftonline.com/common/oauth2/v2.0/authorize|]
+    , idpTokenEndpoint = [uri|https://login.microsoftonline.com/common/oauth2/v2.0/token|]
     }
 
-newtype AzureADUser = AzureADUser {mail :: Text} deriving (Show, Generic)
+-- | https://learn.microsoft.com/en-us/azure/active-directory/develop/userinfo
+data AzureADUser = AzureADUser
+  { sub :: Text
+  , email :: Text
+  , familyName :: Text
+  , givenName :: Text
+  , name :: Text
+  }
+  deriving (Show, Generic)
 
-instance FromJSON AzureADUser
+instance FromJSON AzureADUser where
+  parseJSON = genericParseJSON defaultOptions {fieldLabelModifier = camelTo2 '_'}
