18d97ba6d3618a15001550c636e9c3a887f6fbcf
Haisheng <freizl@gmail.com>
2023-07-25 07:40:29 -0700

    Add tutorial for using Experiment module (#230)
    Fixes #231

--- hoauth2-tutorial/src/HOAuth2ExperimentTutorial.hs
+++ hoauth2-tutorial/src/HOAuth2ExperimentTutorial.hs
@@ -1,3 +1,4 @@
+{-# LANGUAGE DataKinds #-}
 {-# LANGUAGE DeriveGeneric #-}
 {-# LANGUAGE ImportQualifiedPost #-}
 {-# LANGUAGE OverloadedStrings #-}
@@ -5,7 +6,7 @@
 
 -- | If you're hurry, go check source code directly.
 --
--- = Configure your OAuth2 provider
+-- = Configure your Identity Provider and Application
 --
 -- Pick which OAuth2 provider you'd to use, e.g. Google, Github, Auth0 etc.
 -- Pretty much all standard OAuth2 provider has developer portal to guide developer to use oauth2 flow.
@@ -20,15 +21,23 @@
 -- https:\/\/BASE_DOMAIN\/.well-known\/openid-configuration
 -- @
 --
--- In this tutorial, I choose Auth0, which is one of existing OAuth2/OIDC Providers in the market.
--- This is the API Docs <https://auth0.com/docs/api>
+-- In this tutorial, I choose Auth0. This is the API Docs <https://auth0.com/docs/api>.
+--
+-- Define a @Idp@ and an Application
+--
+-- @
+-- auth0 :: Idp "auth0"
+-- auth0 = Idp {...}
+--
+-- auth0AuthCodeApp :: AuthorizationCodeApplication
+-- auth0AuthCodeApp = AuthorizationCodeApplication {...}
+-- @
 --
 -- = Generate Authorization URL.
 --
 -- OAuth2 starts with [authorization](https://www.rfc-editor.org/rfc/rfc6749#section-4).
 --
--- To generate an authorization URL, call method `authorizationUrl`, then call `appendQueryParams` to
--- append additional query parameters, e.g. @state@, @scope@ etc.
+-- To generate an authorization URL, call method `mkAuthorizationRequest`.
 --
 -- That method will also automatically append following query parameter to the authorization url.
 --
@@ -41,7 +50,7 @@
 -- The generated URL looks like
 --
 -- @
--- https://DOMAIN/path/to/authorize?client_id=xxx&response_type=code&redirect_uri=xxx&state=xxx&scope=xxx&..
+-- https://IDP_DOMAIN/path/to/authorize?client_id=xxx&response_type=code&redirect_uri=xxx&state=xxx&scope=xxx&..
 -- @
 --
 -- /Notes/: As of today, @hoauth2@ only supports @Code Grant@.
@@ -50,7 +59,7 @@
 --
 -- Now you need to have your user to navigate to that URL to kick off OAuth flow.
 --
--- There are different ways to redirect user to the authorization URL.
+-- There are different ways to redirect user to this authorization URL.
 --
 -- e.g.
 --
@@ -60,31 +69,33 @@
 --
 -- In this tutorial, I choose the second option. For instance this is how @indexH@ is implemented.
 --
--- >>> setHeader "Location" (uriToText authorizeUrl)
--- >>> status status302
+-- @
+-- setHeader "Location" (uriToText authorizeUrl)
+-- status status302
+-- @
 --
 -- = Obtain Access Token
 --
--- When user navigates to 'authorizeUrl', user will be prompt for login against the OAuth provider.
+-- When user navigates to authorization URL, user will be prompt for login against the OAuth provider.
 --
 -- After an successful login there, user will be redirect back to your Application's @redirect_uri@
 -- with @code@ in the query parameter.
 --
 -- With this @code@, we could exchange for an Access Token.
 --
--- Also you'd better to validate the @state@ is exactly what you pass in the 'authorizeUrl'.
+-- Also you'd better to validate the @state@ is exactly what you pass in the authorization URL.
 -- OAuth2 provider expects to send the exact @state@ back in the redirect request.
 --
--- To obtain an Access Token, you could call 'fetchAccessToken',
+-- To obtain an Access Token, you could call `conduitTokenRequest`,
 -- which essentially takes the authorization @code@, make request to OAuth2 provider's @/token@ endpoint
--- to get an Access Token, plus some other information (see details at 'OAuth2Token').
+-- to get an Access Token.
 --
--- 'fetchAccessToken' returns @ExceptT (OAuth2Error Errors) m OAuth2Token@
+-- `conduitTokenRequest` returns @ExceptT (OAuth2Error Errors) m OAuth2Token@
 -- However Scotty, which is web framework I used to build this tutorial,
--- requires error as Text hence the transform with 'oauth2ErrorToText'
+-- requires error as Text hence it is transformed with @oauth2ErrorToText@
 --
--- Once we got the 'OAuth2Token' (which actually deserves an better name like @TokenResponse@),
--- we could get the actual 'accessToken' of out it, use which to make API requests to resource server (often time same as the authorization server)
+-- Once we got the `OAuth2Token` (which actually deserves an better name like @TokenResponse@),
+-- we could get the actual `accessToken` of out it, use which to make API requests to resource server (often time same as the authorization server)
 --
 -- "Network.OAuth.OAuth2.HttpClient" provides a few handy method to send such API request.
 -- For instance,
@@ -94,7 +105,7 @@
 -- authPostJSON  -- Similar but does POST request
 -- @
 --
--- In this tutorial, it makes request to 'auth0UserInfoUri' to fetch Auth0 user information
+-- In this tutorial, it makes request to @/userinfo@ endpoint to fetch Auth0 user information
 -- so application knows who did the authorize.
 --
 -- = The end
@@ -105,29 +116,24 @@
 -- which provides a nice guide regarding what is OAuth2 and various use cases.
 module Main where
 
-import Control.Monad (void)
+import Control.Monad
 import Control.Monad.IO.Class (liftIO)
 import Control.Monad.Trans.Except
 import Data.Aeson (FromJSON)
-import Data.ByteString.Char8 qualified as BS
 import Data.ByteString.Lazy.Char8 qualified as BSL
 import Data.IORef (IORef, newIORef, readIORef, writeIORef)
-import Data.Text.Encoding qualified as T
+import Data.Map.Strict qualified as Map
+import Data.Set qualified as Set
 import Data.Text.Lazy qualified as TL
 import GHC.Generics (Generic)
 import Network.HTTP.Conduit (newManager, tlsManagerSettings)
 import Network.HTTP.Types (status302)
 import Network.OAuth.OAuth2 (
   ExchangeToken (ExchangeToken),
-  OAuth2 (..),
   OAuth2Token (accessToken),
   TokenResponseError,
-  appendQueryParams,
-  authGetJSON,
-  authorizationUrl,
-  fetchAccessToken,
  )
-import URI.ByteString (URI, serializeURIRef')
+import Network.OAuth2.Experiment
 import URI.ByteString.QQ (uri)
 import Web.Scotty (ActionM, scotty)
 import Web.Scotty qualified as Scotty
@@ -138,32 +144,38 @@ import Web.Scotty qualified as Scotty
 
 ------------------------------
 
-auth0 :: OAuth2
+auth0 :: Idp "auth0"
 auth0 =
-  OAuth2
-    { oauth2ClientId = "TZlmNRtLY9duT8M4ztgFBLsFA66aEoGs"
-    , oauth2ClientSecret = ""
-    , oauth2AuthorizeEndpoint = [uri|https://freizl.auth0.com/authorize|]
-    , oauth2TokenEndpoint = [uri|https://freizl.auth0.com/oauth/token|]
-    , oauth2RedirectUri = [uri|http://localhost:9988/oauth2/callback|]
+  Idp
+    { idpAuthorizeEndpoint = [uri|https://freizl.auth0.com/authorize|]
+    , idpTokenEndpoint = [uri|https://freizl.auth0.com/oauth/token|]
+    , idpUserInfoEndpoint = [uri|http://freizl.auth0.com/userinfo|]
+    , idpDeviceAuthorizationEndpoint = Just [uri|http://freizl.auth0.com/oauth/device/code|]
     }
 
-authorizeUrl :: URI
-authorizeUrl =
-  appendQueryParams
-    [ ("scope", "openid profile email")
-    , ("state", randomStateValue)
-    ]
-    $ authorizationUrl auth0
+authCodeApp :: AuthorizationCodeApplication
+authCodeApp =
+  AuthorizationCodeApplication
+    { acClientId = ""
+    , acClientSecret = ""
+    , acScope = Set.fromList ["openid", "profile", "email", "offline_access"]
+    , acAuthorizeState = randomStateValue
+    , acRedirectUri = [uri|http://localhost:9988/oauth2/callback|]
+    , acName = "sample-auth0-authorization-code-app"
+    , acAuthorizeRequestExtraParams = Map.empty
+    , acTokenRequestAuthenticationMethod = ClientSecretBasic
+    }
+
+auth0DemoApp :: IdpApplication "auth0" AuthorizationCodeApplication
+auth0DemoApp = IdpApplication {idp = auth0, application = authCodeApp}
 
 -- | You'll need to find out an better way to create @state@
 -- which is recommended in <https://www.rfc-editor.org/rfc/rfc6749#section-10.12>
-randomStateValue :: BS.ByteString
+randomStateValue :: AuthorizeState
 randomStateValue = "random-state-to-prevent-csrf"
 
--- | Endpoint for fetching user profile using access token
-auth0UserInfoUri :: URI
-auth0UserInfoUri = [uri|https://freizl.auth0.com/userinfo|]
+isSameState :: AuthorizeState -> TL.Text -> Bool
+isSameState state1 = (== state1) . AuthorizeState
 
 -- | Auth0 user
 -- https://auth0.com/docs/api/authentication#get-user-info
@@ -213,7 +225,7 @@ indexH refUser = do
 -- | @/login@ endpoint handler
 loginH :: ActionM ()
 loginH = do
-  Scotty.setHeader "Location" (uriToText authorizeUrl)
+  Scotty.setHeader "Location" (TL.fromStrict $ uriToText $ mkAuthorizationRequest auth0DemoApp)
   Scotty.status status302
 
 -- | @/logout@ endpoint handler
@@ -228,7 +240,9 @@ callbackH refUser = do
   pas <- Scotty.params
 
   excepttToActionM $ do
-    void $ ExceptT $ pure $ paramValue "state" pas
+    stateV <- ExceptT $ pure $ paramValue "state" pas
+    unless (isSameState randomStateValue stateV) $
+      throwE "Unable to validate state"
     codeP <- ExceptT $ pure $ paramValue "code" pas
 
     mgr <- liftIO $ newManager tlsManagerSettings
@@ -237,12 +251,12 @@ callbackH refUser = do
     -- 'oauth2ErrorToText' turns (OAuth2 error) to Text which is the default way
     -- Scotty represents error message
     let code = ExchangeToken $ TL.toStrict codeP
-    tokenResp <- withExceptT oauth2ErrorToText (fetchAccessToken mgr auth0 code)
+    tokenResp <- withExceptT oauth2ErrorToText (conduitTokenRequest auth0DemoApp mgr code)
 
     -- Call API to resource server with Access Token being authentication code.
     -- 'bslToText' exists for similar reason as 'oauth2ErrorToText'
     let at = accessToken tokenResp
-    user <- withExceptT bslToText (authGetJSON mgr at auth0UserInfoUri)
+    user <- withExceptT bslToText (conduitUserInfoRequest auth0DemoApp mgr at)
 
     -- Now we need to find way to set authentication status for this application
     -- that indicates user has been authenticated successfully.
@@ -257,9 +271,6 @@ callbackH refUser = do
 -- * Utilities
 
 ------------------------------
-
-uriToText :: URI -> TL.Text
-uriToText = TL.fromStrict . T.decodeUtf8 . serializeURIRef'
 
 bslToText :: BSL.ByteString -> TL.Text
 bslToText = TL.pack . BSL.unpack
@@ -280,7 +291,7 @@ paramValue key params =
     hasParam t = (== t) . fst
 
 -- | Lift ExceptT to ActionM which is basically the handler Monad in Scotty.
-excepttToActionM :: Show a => ExceptT TL.Text IO a -> ActionM a
+excepttToActionM :: (Show a) => ExceptT TL.Text IO a -> ActionM a
 excepttToActionM e = do
   result <- liftIO $ runExceptT e
   either Scotty.raise pure result
